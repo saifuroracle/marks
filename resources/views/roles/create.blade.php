cc1103989836bdec8ae51c4b24b82e12bc083330
Mohammad Saifur Rahman <saifur.rahman@sslwireless.com>
2021-11-22 12:49:55 +0600

    auth

--- resources/views/roles/create.blade.php
+++ resources/views/roles/create.blade.php
@@ -6,42 +6,26 @@
 <div class="row">
 
     <div class="col-lg-12 margin-tb">
-
         <div class="pull-left">
-
             <h2>Create New Role</h2>
-
         </div>
-
         <div class="pull-right">
-
             <a class="btn btn-primary" href="{{ route('roles.index') }}"> Back</a>
-
         </div>
-
     </div>
-
 </div>
 
 
 @if (count($errors) > 0)
 
     <div class="alert alert-danger">
-
         <strong>Whoops!</strong> There were some problems with your input.<br><br>
-
         <ul>
-
-        @foreach ($errors->all() as $error)
-
-            <li>{{ $error }}</li>
-
-        @endforeach
-
+            @foreach ($errors->all() as $error)
+                <li>{{ $error }}</li>
+            @endforeach
         </ul>
-
     </div>
-
 @endif
 
 
