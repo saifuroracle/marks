128d3df00a61cfecb21e6566ce4fa94b9328adc3
Mohammad Saifur Rahman <saifur.rahman@sslwireless.com>
2021-11-22 17:58:24 +0600

    auth

--- app/Models/User.php
+++ app/Models/User.php
@@ -13,11 +13,7 @@ class User extends Authenticatable
 {
     use HasApiTokens, HasFactory, Notifiable, HasRoles;
 
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var string[]
-     */
+
     protected $fillable = [
         'name',
         'email',
