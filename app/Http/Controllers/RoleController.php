7b9ca00036734011ca8d2f58ce0da53dd24e0702
Mohammad Saifur Rahman <saifur.rahman@sslwireless.com>
2021-11-22 12:41:46 +0600

    auth

--- app/Http/Controllers/RoleController.php
+++ app/Http/Controllers/RoleController.php
@@ -32,7 +32,6 @@ class RoleController extends Controller
      */
 
     function __construct()
-
     {
         $this->middleware('permission:role-list|role-create|role-edit|role-delete', ['only' => ['index', 'store']]);
         $this->middleware('permission:role-create', ['only' => ['create', 'store']]);
