fb6445f635f61d3f25e9799b241cf94de9f5a6f4
saifur rahman <saifuroracle@gmail.com>
2021-11-22 23:29:40 +0600

    updated

--- routes/web.php
+++ routes/web.php
@@ -20,24 +20,25 @@ Route::post('/loginPost',  [AuthController::class, 'loginPost'])->name('loginPos
 Route::get('/home', [HomeController::class, 'index'])->name('home');
 
 
-// Route::group(['middleware' => ['auth']], function() {
-//     // Route::resource('roles', RoleController::class);
-//     Route::resource('users', UserController::class);
-// });
 
-// manage permission routes
-Route::get('/manage-permissions', [PermissionController::class, 'managepermissions'])->name('managepermissions');
-Route::post('/createpermissionsave', [PermissionController::class, 'createpermissionsave'])->name('createpermissionsave');
-Route::post('/editpermissionsave', [PermissionController::class, 'editpermissionsave'])->name('editpermissionsave');
-Route::post('/deletepermission', [PermissionController::class, 'deletepermission'])->name('deletepermission');
+Route::group(['middleware' => ['auth']], function() {
 
-// manage role routes
-Route::get('/manage-roles', [RoleController::class, 'manageroles'])->name('manageroles');
-Route::post('/createrolesave', [RoleController::class, 'createrolesave'])->name('createrolesave');
-Route::post('/editrolesave', [RoleController::class, 'editrolesave'])->name('editrolesave');
-Route::post('/deleterole', [RoleController::class, 'deleterole'])->name('deleterole');
+    // manage permission routes
+    Route::get('/manage-permissions', [PermissionController::class, 'managepermissions'])->name('managepermissions');
+    Route::post('/createpermissionsave', [PermissionController::class, 'createpermissionsave'])->name('createpermissionsave');
+    Route::post('/editpermissionsave', [PermissionController::class, 'editpermissionsave'])->name('editpermissionsave');
+    Route::post('/deletepermission', [PermissionController::class, 'deletepermission'])->name('deletepermission');
 
-// manage user routes
-Route::get('/manage-users', [UserController::class, 'manageusers'])->name('manageusers');
-Route::post('/createusersave', [UserController::class, 'createusersave'])->name('createusersave');
-Route::post('/editusersave', [UserController::class, 'editusersave'])->name('editusersave');
+    // manage role routes
+    Route::get('/manage-roles', [RoleController::class, 'manageroles'])->name('manageroles');
+    Route::post('/createrolesave', [RoleController::class, 'createrolesave'])->name('createrolesave');
+    Route::post('/editrolesave', [RoleController::class, 'editrolesave'])->name('editrolesave');
+    Route::post('/deleterole', [RoleController::class, 'deleterole'])->name('deleterole');
+
+    // manage user routes
+    Route::get('/manage-users', [UserController::class, 'manageusers'])->name('manageusers');
+    Route::post('/createusersave', [UserController::class, 'createusersave'])->name('createusersave');
+    Route::post('/editusersave', [UserController::class, 'editusersave'])->name('editusersave');
+
+});
+
