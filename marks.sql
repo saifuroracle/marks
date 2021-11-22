9aa09914e5c2a32e698d76893bbddfda3c8ef47e
saifur rahman <saifuroracle@gmail.com>
2021-11-22 22:26:33 +0600

    updated

--- marks.sql
+++ marks.sql
@@ -1,5 +1,5 @@
 /*
-SQLyog Professional v13.1.1 (64 bit)
+SQLyog Ultimate v13.1.1 (64 bit)
 MySQL - 10.4.8-MariaDB : Database - marks
 *********************************************************************
 */
@@ -47,7 +47,7 @@ CREATE TABLE `migrations` (
 
 /*Data for the table `migrations` */
 
-insert  into `migrations`(`id`,`migration`,`batch`) values 
+insert  into `migrations`(`id`,`migration`,`batch`) values
 (1,'2014_10_12_000000_create_users_table',1),
 (2,'2014_10_12_100000_create_password_resets_table',1),
 (3,'2019_08_19_000000_create_failed_jobs_table',1),
@@ -77,17 +77,20 @@ CREATE TABLE `model_has_roles` (
   `role_id` bigint(20) unsigned NOT NULL,
   `model_type` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
   `model_id` bigint(20) unsigned NOT NULL,
-  `deleted_at` timestamp NULL DEFAULT NULL,
-  PRIMARY KEY (`role_id`,`model_id`,`model_type`),
-  CONSTRAINT `model_has_roles_role_id_foreign` FOREIGN KEY (`role_id`) REFERENCES `roles` (`id`) ON DELETE CASCADE
+  `deleted_at` timestamp NULL DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 /*Data for the table `model_has_roles` */
 
-insert  into `model_has_roles`(`role_id`,`model_type`,`model_id`,`deleted_at`) values 
-(7,'App\\Models\\User',1,NULL),
-(7,'App\\Models\\User',2,'2021-11-22 17:53:24'),
-(8,'App\\Models\\User',2,NULL);
+insert  into `model_has_roles`(`role_id`,`model_type`,`model_id`,`deleted_at`) values
+(11,'App\\Models\\User',2,'2021-11-22 22:21:55'),
+(11,'App\\Models\\User',2,'2021-11-22 22:21:55'),
+(11,'App\\Models\\User',2,'2021-11-22 22:21:55'),
+(11,'App\\Models\\User',2,'2021-11-22 22:21:55'),
+(11,'App\\Models\\User',2,'2021-11-22 22:21:55'),
+(11,'App\\Models\\User',2,'2021-11-22 22:21:55'),
+(11,'App\\Models\\User',2,'2021-11-22 22:21:55'),
+(11,'App\\Models\\User',2,NULL);
 
 /*Table structure for table `password_resets` */
 
@@ -118,7 +121,7 @@ CREATE TABLE `permissions` (
 
 /*Data for the table `permissions` */
 
-insert  into `permissions`(`id`,`name`,`guard_name`,`created_at`,`updated_at`,`deleted_at`) values 
+insert  into `permissions`(`id`,`name`,`guard_name`,`created_at`,`updated_at`,`deleted_at`) values
 (1,'role list','web',NULL,'2021-11-22 13:20:09',NULL),
 (2,'role data','web',NULL,'2021-11-22 13:20:58',NULL),
 (3,'role create','web',NULL,'2021-11-22 14:49:21',NULL),
@@ -158,45 +161,18 @@ CREATE TABLE `role_has_permissions` (
 
 /*Data for the table `role_has_permissions` */
 
-insert  into `role_has_permissions`(`permission_id`,`role_id`,`deleted_at`) values 
-(2,7,'2021-11-22 17:31:20'),
-(2,8,'2021-11-22 18:01:38'),
-(2,7,'2021-11-22 17:31:20'),
-(2,8,'2021-11-22 18:01:38'),
-(2,8,'2021-11-22 18:01:38'),
-(3,8,'2021-11-22 18:01:38'),
-(2,8,'2021-11-22 18:01:38'),
-(5,8,'2021-11-22 18:01:38'),
-(1,8,'2021-11-22 18:01:38'),
-(4,8,'2021-11-22 18:01:38'),
-(3,7,NULL),
-(2,7,NULL),
-(5,7,NULL),
-(1,7,NULL),
-(4,7,NULL),
-(3,8,'2021-11-22 18:01:38'),
-(2,8,'2021-11-22 18:01:38'),
-(5,8,'2021-11-22 18:01:38'),
-(1,8,'2021-11-22 18:01:38'),
-(3,8,'2021-11-22 18:01:38'),
-(2,8,'2021-11-22 18:01:38'),
-(5,8,'2021-11-22 18:01:38'),
-(1,8,'2021-11-22 18:01:38'),
-(3,8,'2021-11-22 18:01:38'),
-(2,8,'2021-11-22 18:01:38'),
-(5,8,'2021-11-22 18:01:38'),
-(1,8,'2021-11-22 18:01:38'),
-(3,8,'2021-11-22 18:01:38'),
-(2,8,'2021-11-22 18:01:38'),
-(5,8,'2021-11-22 18:01:38'),
-(1,8,'2021-11-22 18:01:38'),
-(3,8,'2021-11-22 18:01:38'),
-(2,8,'2021-11-22 18:01:38'),
-(5,8,'2021-11-22 18:01:38'),
-(3,8,'2021-11-22 18:01:38'),
-(2,8,'2021-11-22 18:01:38'),
-(3,8,'2021-11-22 18:01:38'),
-(2,8,NULL);
+insert  into `role_has_permissions`(`permission_id`,`role_id`,`deleted_at`) values
+(2,10,'2021-11-22 21:06:27'),
+(3,11,'2021-11-22 22:16:33'),
+(2,11,'2021-11-22 22:16:33'),
+(5,11,'2021-11-22 22:16:33'),
+(1,11,'2021-11-22 22:16:33'),
+(4,11,'2021-11-22 22:16:33'),
+(3,11,NULL),
+(2,11,NULL),
+(5,11,NULL),
+(1,11,NULL),
+(4,11,NULL);
 
 /*Table structure for table `roles` */
 
@@ -210,13 +186,13 @@ CREATE TABLE `roles` (
   `updated_at` timestamp NULL DEFAULT NULL ON UPDATE current_timestamp(),
   `deleted_at` timestamp NULL DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=12 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 /*Data for the table `roles` */
 
-insert  into `roles`(`id`,`name`,`guard_name`,`created_at`,`updated_at`,`deleted_at`) values 
-(7,'Admin','web','2021-11-22 11:26:38','2021-11-22 11:27:00',NULL),
-(8,'test 2','web','2021-11-22 11:26:50','2021-11-22 18:04:00',NULL);
+insert  into `roles`(`id`,`name`,`guard_name`,`created_at`,`updated_at`,`deleted_at`) values
+(10,'test','web','2021-11-22 15:06:12','2021-11-22 15:06:27','2021-11-22 21:06:27'),
+(11,'Admin','web','2021-11-22 16:15:13','2021-11-22 16:15:13',NULL);
 
 /*Table structure for table `users` */
 
@@ -238,9 +214,9 @@ CREATE TABLE `users` (
 
 /*Data for the table `users` */
 
-insert  into `users`(`id`,`name`,`email`,`email_verified_at`,`password`,`remember_token`,`created_at`,`updated_at`,`status`) values 
+insert  into `users`(`id`,`name`,`email`,`email_verified_at`,`password`,`remember_token`,`created_at`,`updated_at`,`status`) values
 (1,'admin','admin@gmail.com',NULL,'$2y$10$RKMvbp1Rz4gxUkegSEcQUOZIIgpVMZM4erA34FWxzFp2CBulGRo6q','Ujdr8cRgaYb5zyyrSI37e5FgBOCmJPiIkqrZU8279ztR9EAC6HmP5lHhEdo7','2021-11-22 05:53:08','2021-11-22 05:53:08',1),
-(2,'test','test@mail.com',NULL,'$2y$10$bAQrp4gZA9JjurY60EUXyu3vu/sUdHCFeTMKW/tjdglgaWpO33p1C',NULL,'2021-11-22 11:44:51','2021-11-22 11:44:51',1);
+(2,'test222','test@mail.com',NULL,'$2y$10$bAQrp4gZA9JjurY60EUXyu3vu/sUdHCFeTMKW/tjdglgaWpO33p1C',NULL,'2021-11-22 11:44:51','2021-11-22 16:21:55',1);
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
 /*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
